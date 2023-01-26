4d140ee05d78278efdb91c45fb05626bf9a66f4f
Samy Rjob <s.rjob@audencia.com>
2023-01-26 03:21:50 +0100

    il manque les images

--- app/controllers/student/dashboards_controller.rb
+++ app/controllers/student/dashboards_controller.rb
@@ -2,7 +2,7 @@ class Student::DashboardsController < ApplicationController
   before_action :set_student, only: [:show]
 
   def show
-
+    
     @applications = @student.applications
     @savedoffers = Savedoffer.where(student_id: @student.id)
   end
