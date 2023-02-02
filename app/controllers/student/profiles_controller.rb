cc09ee9e64e42af08738f399d018feac752320f6
Samy Rjob <s.rjob@audencia.com>
2023-02-02 19:26:40 +0100

    jej

--- app/controllers/student/profiles_controller.rb
+++ app/controllers/student/profiles_controller.rb
@@ -1,13 +1,29 @@
 class Student::ProfilesController < ApplicationController
 
+
   def show
     @student = current_student
   end
 
-  def update
-    @student = current_student
-    redirect_to student_profile_path(@student)
-  end
+  # def update
+  #   @student = current_student
+
+
+  #   if @student.update(update_params)
+  #     redirect_to student_profile_path(@student)
+  #   else
+  #     flash[:errors] = @student.errors.full_messages
+  #     redirect_to student_profile_path(@student)
+  #   end
+
+  # end
+
+
+  # private
+
+  # def update_params
+  #   params.require(:student).permit(:first_name, :last_name, :description, :profile, :school)
+  # end
 
 
 
