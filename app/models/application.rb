6ac7ad00bdc43d0f7dd8b12eaa90f5f5592ff998
Samy Rjob <s.rjob@audencia.com>
2023-02-14 10:45:47 +0100

    eyhee

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,6 @@ class Application < ApplicationRecord
   belongs_to :offer
   belongs_to :student
   validates :description, presence: false
-  has_many_attached :dossiers
+  has_many_attached :images
   validates :status, inclusion: { in: %w[accepted pending declined kept nokept] }
 end
