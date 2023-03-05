90f11b478438bfe5c38d3b21b2159c5c1229a60f
Samy Rjob <s.rjob@audencia.com>
2023-03-05 18:48:26 +0100

    heheheh

--- app/controllers/student/offers_controller.rb
+++ app/controllers/student/offers_controller.rb
@@ -62,9 +62,9 @@ class Student::OffersController < ApplicationController
     # If an old id or a numeric id was used to find the record, then
     # the request path will not match the post_path, and we should do
     # a 301 redirect that uses the current friendly id.
-    if request.path != offer_path(@offer)
-      return redirect_to @offer, :status => :moved_permanently
-    end
+    # if request.path != offer_path(@offer)
+    #   return redirect_to @offer, :status => :moved_permanently
+    # end
   end
 
 end
