ad8fa66c83d6fe6b468d5394132b517b96fe5aa2
Samy Rjob <s.rjob@audencia.com>
2023-03-05 18:17:20 +0100

    heheh

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
+  extend FriendlyId
   belongs_to :company
   has_many :applications, dependent: :destroy
   has_many :savedoffers, dependent: :destroy
@@ -14,7 +15,9 @@ class Offer < ApplicationRecord
   # as: :record
 
   # self.per_page = 7
-  extend FriendlyId
-  friendly_id :function, use: [:slugged, :finders]
+  friendly_id :function, use: [:slugged]
 
+  def should_generate_new_friendly_id?
+    function_changed? || slug.blank?
+  end
 end
