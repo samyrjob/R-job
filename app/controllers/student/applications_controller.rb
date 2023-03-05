1fb1a251e11affc53203f5cab76ad261d8b92cad
Samy Rjob <s.rjob@audencia.com>
2023-03-05 18:57:55 +0100

    jeej

--- app/controllers/student/applications_controller.rb
+++ app/controllers/student/applications_controller.rb
@@ -1,8 +1,9 @@
 class Student::ApplicationsController < ApplicationController
 
+  before_action :find_offer, only: %i[create new]
 
   def create
-    @offer = Offer.find(params[:offer_id])
+    # @offer = Offer.find(params[:offer_id])
     @application = Application.new(application_params)
     @application.student = current_student # user est associé à la création du booking (renter vient de la db)
     @application.offer = @offer
@@ -18,7 +19,7 @@ class Student::ApplicationsController < ApplicationController
 
   def new
     @application = Application.new
-    @offer = Offer.find(params[:offer_id])
+    # @offer = Offer.find(params[:offer_id])
   end
 
 
@@ -28,5 +29,18 @@ private
   def application_params
     params.require(:application).permit(:description, :student_id, :offer_id, images: [])   # A configurer plutard
   end
+
+
+  def find_offer
+    @offer = Offer.friendly.find(params[:id])
+
+    # If an old id or a numeric id was used to find the record, then
+    # the request path will not match the post_path, and we should do
+    # a 301 redirect that uses the current friendly id.
+    # if request.path != offer_path(@offer)
+    #   return redirect_to @offer, :status => :moved_permanently
+    # end
+  end
+
 
 end
--- app/controllers/student/savedoffers_controller.rb
+++ app/controllers/student/savedoffers_controller.rb
@@ -1,11 +1,9 @@
 class Student::SavedoffersController < ApplicationController
-
-
-
+  before_action :find_offer, only: %i[create update]
 
 
   def create
-    @offer = Offer.find(params[:offer_id])
+    # @offer = Offer.find(params[:offer_id])
     @savedoffer = Savedoffer.new(savedoffer_params)
     @savedoffer.saved = true
     @savedoffer.offer = @offer
@@ -15,7 +13,7 @@ class Student::SavedoffersController < ApplicationController
   end
 
   def update
-    @offer = Offer.find(params[:offer_id])
+    # @offer = Offer.find(params[:offer_id])
     @savedoffer = Savedoffer.find(params[:id])
 
     if @savedoffer.saved == true
@@ -41,6 +39,16 @@ class Student::SavedoffersController < ApplicationController
     params.require(:savedoffer).permit(:saved, :student_id, :offer_id)
   end
 
+  def find_offer
+    @offer = Offer.friendly.find(params[:id])
+
+    # If an old id or a numeric id was used to find the record, then
+    # the request path will not match the post_path, and we should do
+    # a 301 redirect that uses the current friendly id.
+    # if request.path != offer_path(@offer)
+    #   return redirect_to @offer, :status => :moved_permanently
+    # end
+  end
 
 
 end
