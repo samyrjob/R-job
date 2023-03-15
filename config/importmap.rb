6e4a968c8b31d1b467ac18cd30cb9933d3703651
Samy Rjob <s.rjob@audencia.com>
2023-03-15 12:52:12 +0100

    uifhzguizghb

--- config/importmap.rb
+++
@@ -1,8 +0,0 @@
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", proload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", proload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.min.js", proload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "slim-select", to: "https://ga.jspm.io/npm:slim-select@1.27.1/dist/slimselect.min.mjs"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.js"
