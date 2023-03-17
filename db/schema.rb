18a3cbf32474a690b18463bd686bba4b56557205
Samy Rjob <s.rjob@audencia.com>
2023-03-17 20:00:17 +0100

    yehfhfhe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_155856) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_185644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_155856) do
     t.text "description"
     t.string "structure"
     t.string "website"
+    t.boolean "admin", default: false, null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -118,7 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_155856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "town"
-    t.float "salary"
+    t.string "salary"
     t.date "start_date"
     t.date "end_activation"
     t.string "status"
