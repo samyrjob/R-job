5a6af5b442c886f004549e8958cb8ca3bc9a0bf2
Samy Rjob <s.rjob@audencia.com>
2023-03-22 16:09:47 +0100

    jee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
 
   # config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://traineenandplus.com" }
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   location: '/usr/sbin/sendmail',
